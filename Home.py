1809fa75ca285e6d012b541752300a1c56157a16
angelouis <alouis001@citymail.cuny.edu>
2022-11-30 00:37:21 -0500

    Added home page, title, and a picture

---
+++ Home.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.header("NYC Parking Ticket Data Visualizer! 👋")
+
+st.image('images/nyc_map_image.jpg')
