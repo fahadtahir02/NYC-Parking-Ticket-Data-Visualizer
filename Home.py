a72f814fb91f34e424f22cfc28eb266df6feae6a
angelouis <alouis001@citymail.cuny.edu>
2022-12-13 17:06:43 -0500

    Fixed home page

--- Home.py
+++ Home.py
@@ -4,10 +4,13 @@ import numpy as np
 
 
 st.set_page_config(
-    page_title="Hello",
-    page_icon="👋",
+    page_title="NYC Parking Ticket Data Visualizer",
+    page_icon="🚗",
 )
 
-st.header("NYC Parking Ticket Data Visualizer! 👋")
+st.header("🚗 NYC Parking Ticket Data Visualizer! 🚗")
 
 st.image('images/nyc_map_image.jpg')
+
+
+st.write('We are creating data visualizations of NYC Parking Tickets data to analyze and draw conclusions. We want to see if there are any patterns we can identify in this data. The data we are using is around 30,000 rows of tickets that includes information such as Registration State, Violation Code, Violation Time, etc. Navigate to the Visualizations page to interact with the different models we made. Navigate tot he License page to interact with our dataset.')
